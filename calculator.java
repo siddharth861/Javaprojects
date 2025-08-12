f27e0e99f489a8bd1f68a3ccf707a28421177a5e
siddharth861 <pandeysiddharth861@gmail.com>
2025-08-12 21:09:57 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+// calculator using enhanced switches
+public class calculator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        char operator;
+        double result=0;
+        boolean validOperation = true;
+
+        System.out.print("Enter the 1st number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter the operator(+,_,*,/,^): ");
+        operator= scanner.next().charAt(0);
+
+        System.out.print("Enter the 2nd number: ");
+        num2 = scanner.nextDouble();
+
+        switch(operator){
+            case '+' -> result = num1 +num2;
+            case '-' -> result = num1 - num2;
+            case'*' -> result = num1*num2;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println("Cannot divide by zero");
+                    validOperation = false;
+                }
+                else{
+                    result = num1/num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1,num2);
+
+            default -> {
+                System.out.println("Enter an operator from the options given");
+                validOperation = false;
+            }
+        }
+        if(validOperation){
+            System.out.println(result);
+        }
+
+    }
+}
